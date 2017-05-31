2b47ddac3922a13f9fac975b6d23be0e7e9a4b82
jacob ruleaux and max scher <jacob@>
2017-05-31 10:17:21 -0700

    add all cards with wells

--- js/scripts.js
+++ js/scripts.js
@@ -27,6 +27,52 @@ $(document).ready(function() {
     $(".card-show-arg").slideToggle();
     $(".card-hide-arg").slideToggle();
   });
-
-
+  $(".click-par").click(function() {
+    $(".card-show-par").slideToggle();
+    $(".card-hide-par").slideToggle();
+  });
+  $(".click-rtrn").click(function() {
+    $(".card-show-rtrn").slideToggle();
+    $(".card-hide-rtrn").slideToggle();
+  });
+  $(".click-chain").click(function() {
+      $(".card-show-chain").slideToggle();
+      $(".card-hide-chain").slideToggle();
+  });
+  $(".click-str").click(function() {
+      $(".card-show-str").slideToggle();
+      $(".card-hide-str").slideToggle();
+  });
+  $(".click-boo").click(function() {
+      $(".card-show-boo").slideToggle();
+      $(".card-hide-boo").slideToggle();
+    });
+  $(".click-und").click(function() {
+      $(".card-show-und").slideToggle();
+      $(".card-hide-und").slideToggle();
+    });
+  $(".click-NaN").click(function() {
+      $(".card-show-NaN").slideToggle();
+      $(".card-hide-NaN").slideToggle();
+    });
+  $(".click-esc").click(function() {
+      $(".card-show-esc").slideToggle();
+      $(".card-hide-esc").slideToggle();
+    });
+    $(".click-alrt").click(function() {
+        $(".card-show-alrt").slideToggle();
+        $(".card-hide-alrt").slideToggle();
+      });
+    $(".click-comm").click(function() {
+        $(".card-show-comm").slideToggle();
+        $(".card-hide-comm").slideToggle();
+      });
+    $(".click-jqry").click(function() {
+        $(".card-show-jqry").slideToggle();
+        $(".card-hide-jqry").slideToggle();
+      });
+    $(".click-attr").click(function() {
+        $(".card-show-attr").slideToggle();
+        $(".card-hide-attr").slideToggle();
+      });
 });

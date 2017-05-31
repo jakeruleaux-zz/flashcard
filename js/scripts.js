c0d8a77941c19af95845462a6d6979e6347994a7
jacob ruleaux and max scher <jacob@>
2017-05-31 09:17:13 -0700

    add rules for card function

---
+++ js/scripts.js
@@ -0,0 +1,13 @@
+$(document).ready(function() {
+  $(".click-JS").click(function() {
+    $(".card-show-js").slideToggle();
+    $(".card-hide-js").slideToggle();
+  });
+  $(".click-ops").click(function() {
+    $(".card-show-ops").slideToggle();
+    $(".card-hide-ops").slideToggle();
+  });
+
+
+
+});

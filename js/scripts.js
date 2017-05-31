f2fc7319d47e178b2142409d7aa90973c847200b
jacob ruleaux and max scher <jacob@>
2017-05-31 09:34:29 -0700

    add 5 new cards to grid

--- js/scripts.js
+++ js/scripts.js
@@ -7,7 +7,26 @@ $(document).ready(function() {
     $(".card-show-ops").slideToggle();
     $(".card-hide-ops").slideToggle();
   });
-
+  $(".click-var").click(function() {
+    $(".card-show-var").slideToggle();
+    $(".card-hide-var").slideToggle();
+  });
+  $(".click-vnc").click(function() {
+    $(".card-show-vnc").slideToggle();
+    $(".card-hide-vnc").slideToggle();
+  });
+  $(".click-func").click(function() {
+    $(".card-show-func").slideToggle();
+    $(".card-hide-func").slideToggle();
+  });
+  $(".click-meth").click(function() {
+    $(".card-show-meth").slideToggle();
+    $(".card-hide-meth").slideToggle();
+  });
+  $(".click-arg").click(function() {
+    $(".card-show-arg").slideToggle();
+    $(".card-hide-arg").slideToggle();
+  });
 
 
 });
